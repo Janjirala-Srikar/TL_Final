88cbd288fcb4e411b7bc2357967f38bd0b8c36fd
ohsoKool <rishikeshgunda9@gmail.com>
2025-07-07 20:48:09 +0530

    Removed frontend folder

--- frontend/src/components/ScrollProgress.jsx
+++
@@ -1,9 +0,0 @@
-import { useState, useEffect } from 'react';
-import { motion } from 'framer-motion';
-
-const ScrollProgress = () => {
-  // No custom progress bar - just return null to remove it completely
-  return null;
-};
-
-export default ScrollProgress;
