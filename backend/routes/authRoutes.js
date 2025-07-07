161440e1914b8f9c8406c82a92ac851c508c98a3
Srikar <srikarjanjirala92@gmail.com>
2025-07-07 12:44:02 +0530

    Fixed AUTH issues

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -34,13 +34,11 @@ router.post('/register', async function register(req, res) {
       return res.status(400).json({ message: 'User already exists' });
     }
 
-    const hash = await bcrypt.hash(password, SALT_ROUNDS);
-
     const newUser = await User.create({
       firstName,
       lastName,
       email,
-      password: hash
+      password
     });
 
     const token = generateToken(newUser._id);
